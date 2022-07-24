76cc1854e14a0e205d2bb92d715d9bd7dae2802b
Noisk8 <juansk8jaramillo@gmail.com>
2022-07-24 09:07:54 -0500

    Update visual.js

--- hydra/visual.js
+++ hydra/visual.js
@@ -1,17 +1,16 @@
-a.show()
-//PARTE 1
-//video1 https://media.giphy.com/media/42jxlCdqD4DIiSW0RR/giphy.mp4
-//video2 https://media.giphy.com/media/AU1urcs8LL6CtjWjIU/giphy.mp4
-//video3 https://media.giphy.com/media/FAu0E1wYIdV62NyiXa/giphy.mp4
-//video4 https://media.giphy.com/media/Z3mCLdAUfsGbAVzA7h/giphy.mp4
-//video5 https://media.giphy.com/media/DRI0tb5ayBAblOWvfM/giphy.mp4
-//video6 https://media.giphy.com/media/oXiqtblz6Kuy9JgN2k/giphy.mp4
-s0.initVideo('https://media.giphy.com/media/FAu0E1wYIdV62NyiXa/giphy.mp4') 
+render(o0)
 
 
-src(s0)
 
-  .luma(() => a.fft[1]*.4)
-.mult(voronoi(.7,.2,.1))
- 
-  .out(o0)
+//@ferdoropeza
+s0.initImage('/home/noisk8/FONDO2.png') // initialize a webcam in source buffer s0
+
+
+src(o0)
+voronoi(7,1,.5)
+.hue(0.4)
+.modulate(s0,0.8)
+.scale(1)
+.luma([0.15,0.25])
+
+.out()
